2b5bc5d43f2b0944e6beda70d8ec796ad242641a
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2023-09-21 16:11:32 -0600

    astro init v3

---
+++ src/lib/contentful.ts
@@ -0,0 +1,21 @@
+import contentful from "contentful";
+import type { EntryFieldTypes } from "contentful";
+
+export const contentfulClient = contentful.createClient({
+    space: import.meta.env.CONTENTFUL_SPACE_ID,
+    accessToken: import.meta.env.DEV
+        ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+        : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+    host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
+
+export interface Blog {
+    contentTypeId: "blogOfTheHappyTacoCompany";
+    fields: {
+        title: EntryFieldTypes.Text;
+        description: EntryFieldTypes.Text;
+        content: EntryFieldTypes.RichText;
+        pubDate: EntryFieldTypes.Date;
+        slug: EntryFieldTypes.Text;
+    };
+}
